e728d28394b738e80cf218ca1614a66279a52a6a
kobrak1 <burakkarhan657@gmail.com>
2024-04-23 17:55:29 +0300

    update: Header component is modified with 3 sub-components called Logo, SearchBar, and DarkModeButton

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,61 +1,27 @@
-import { useContext, useState } from "react";
-import { MainContext } from "../../context/MainProvider";
-import "./Header.css";
+import { useState } from "react"
+import { useMain } from "../../context/MainProvider"
+import Logo from "./HeaderComponents/Logo"
+import SearchBar from "./HeaderComponents/SearchBar"
+import DarkModeButton from "./HeaderComponents/DarkModeButton"
+import "./Header.css"
 
 const Header = () => {
-  const { 
-    darkMode, 
-    handleDarkMode,
-    setCenter
-  } = useContext(MainContext);
-  const [searchItem, setSearchItem] = useState({lat: '', lng: ''})
-
-  // handle search item
-  const handleSearch = () => {
-    const [lat,lng] = searchItem.split(',')
-    const coordinatesObject = {
-      lat: parseFloat(lat),
-      lng: parseFloat(lng)
-    }
-    setCenter(coordinatesObject)
-  }
+  const { setCenter } = useMain()
+  const [searchItem, setSearchItem] = useState('')
 
   return (
     <header>
       <div className="header-wrapper">
-        <div className="logo-container">
-          <h2>
-            <a 
-              href="https://github.com/kobrak1/gps-application" 
-              target="_blank" 
-              title="https://github.com/kobrak1/gps-application"
-            >
-              Location Mapper
-            </a>
-          </h2>
-        </div>
-        <div className="search-container">
-          <input 
-            className="search-input"
-            type="text"
-            value={searchItem}
-            onChange={e => setSearchItem(e.target.value)}
-            placeholder="Search..." 
-          />
-          <button className="search-button" onClick={() => handleSearch(searchItem)}>Search</button>
-        </div>
-        <div className={`dark-mode ${!darkMode === false && "dark"}`}>
-          <span
-            className={`material-symbols-outlined ${!darkMode && "dark"}`}
-            onClick={handleDarkMode}
-            style={{ cursor: "pointer" }}
-          >
-            {darkMode ? "light_mode" : "dark_mode"}
-          </span>
-        </div>
+        <Logo />
+        <SearchBar 
+          searchItem={searchItem} 
+          setSearchItem={setSearchItem} 
+          setCenter={setCenter} 
+        />
+        <DarkModeButton />
       </div>
     </header>
-  );
-};
+  )
+}
 
 export default Header;
