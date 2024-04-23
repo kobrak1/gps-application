a81856f8d8b4f0b8fefaacc1ea6f5ac3203c04bb
kobrak1 <burakkarhan657@gmail.com>
2024-04-23 17:56:18 +0300

    update: ThemeProvider and MainProvider is implemented to main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import MainProvider from './context/MainProvider.jsx'
+import { MainProvider } from './context/MainProvider.jsx'
+import { ThemeProvider } from './context/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainProvider>
-      <App />
-    </MainProvider>
+    <ThemeProvider>
+      <MainProvider>
+        <App />
+      </MainProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
