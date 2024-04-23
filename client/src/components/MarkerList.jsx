83583981b15a769f302432ff169382839506d38b
kobrak1 <burakkarhan657@gmail.com>
2024-04-23 17:56:52 +0300

    chore: code refactored

--- client/src/components/MarkerList.jsx
+++ client/src/components/MarkerList.jsx
@@ -1,36 +1,40 @@
-import PropTypes from 'prop-types'
-import { Button } from '@mui/material'
-import { useContext } from 'react'
-import { MainContext } from '../context/MainProvider'
+import PropTypes from "prop-types"
+import { Button } from "@mui/material"
+import { useContext } from "react"
+import { MainContext } from "../context/MainProvider"
 
 const MarkerList = ({ markers, removeMarker }) => {
-    const { handleItemClick } = useContext(MainContext)
+  const { handleItemClick } = useContext(MainContext)
 
-    return (
-        <div className="markers-container">
-            <h2>Markers</h2>
-            <div className="markers-list">
-                <ul>
-                    {markers.map((item, index) => (
-                        <li key={index}>
-                            <span onClick={() => handleItemClick(item)}> 
-                                <b>Lat:</b> {item.lat}, <b>Lng:</b> {item.lng}
-                            </span>
-                            <Button size='small' variant='outlined' onClick={() => removeMarker(item.id)}>
-                                Remove
-                            </Button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="markers-container">
+      <h2>Markers</h2>
+      <div className="markers-list">
+        <ul>
+          {markers.map((item, index) => (
+            <li key={index}>
+              <span onClick={() => handleItemClick(item)}>
+                <b>Lat:</b> {item.lat}, <b>Lng:</b> {item.lng}
+              </span>
+              <Button
+                size="small"
+                variant="outlined"
+                onClick={() => removeMarker(item.id)}
+              >
+                Remove
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 MarkerList.propTypes = {
-    markers: PropTypes.array.isRequired,
-    removeMarker: PropTypes.func.isRequired,
-    setCenter: PropTypes.func.isRequired
+  markers: PropTypes.array.isRequired,
+  removeMarker: PropTypes.func.isRequired,
+  setCenter: PropTypes.func.isRequired,
 }
 
 export default MarkerList
